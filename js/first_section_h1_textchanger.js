39bcbfb90418c2d33f2388ed0029f0fec0ffbaf2
강은찬 <99608927+Xeron0310@users.noreply.github.com>
2023-04-23 13:44:26 +0900

    first_section 오류수정

--- js/first_section_h1_textchanger.js
+++ js/first_section_h1_textchanger.js
@@ -1,9 +1,9 @@
 const h1 = document.querySelector('.first_section_div_h1');
 
 h1.addEventListener('click', () => {
-    h1.textContent = "Kang Eun Chan's PORTFOLIO";
+    h1.textContent = "『 Kang Eun Chan's PORTFOLIO 』";
 });
 
 h1.addEventListener('mouseout', () => {
-    h1.textContent = 'MY PORTFOLIO';
+    h1.textContent = '『 MY PORTFOLIO 』';
 });
